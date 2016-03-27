63a24e68d9a452a83b755d0ae8cb46fddf7c2940
Raphael <ralmeida54@yahoo.com.br>
2016-03-27 16:04:00 -0300

    Primeiras implementações sobre:
    - efeito no momento em que o elemento aparece no viewport (paralax)
    - one scroll

--- js/portifolio/portifolio.js
+++ js/portifolio/portifolio.js
@@ -4,6 +4,59 @@ $(document).ready(function(){
         $(this).parent("#mainNav").toggleClass("openMainNav");
     });
 
+    //Function which aplly a class when element is visible on the viewport
+    $(window).scroll(function (e) {
+        var scrollTop = $(window).scrollTop();
+        var viewportHeight = $(window).height();
+
+        $('section:nth-child(n+2)').each(function () {
+            var top = $(this).offset().top;
+            var bottom = top + $(this).height();
+
+            if(scrollTop + viewportHeight >= top ){
+                $(this).addClass('visible');
+            } else {
+                console.log(top, bottom, scrollTop, viewportHeight);
+                $(this).removeClass('visible');
+            }
+        });
+    });
+
+
+
+
+    //Function witch scrolls page to the next section
+    //Firefox
+    $(window).bind('DOMMouseScroll', function(e){
+        var vheight = $(window).scrollTop();
+        var sheight = $('section#cover').height();
+        var totalDown = vheight + sheight;
+        var totalUp = vheight - sheight;
+        if(e.originalEvent.detail > 0) {
+            $('body').animate({scrollTop: totalDown}, '1500');
+        }else {
+            $('body').animate({scrollTop: totalUp}, '1500');
+        }
+        //prevent page fom scrolling
+        return false;
+    });
+
+    //Function witch scrolls page to the next section
+    //IE, Opera, Safari
+    $(window).bind('mousewheel', function(e){
+        var vheight = $(window).scrollTop();
+        var sheight = $('section#cover').height();
+        var totalDown = vheight + sheight;
+        var totalUp = vheight - sheight;
+        if(e.originalEvent.wheelDelta < 0) {
+            $('body').animate({scrollTop: totalDown}, '1500');
+        }else {
+            $('body').animate({scrollTop: totalUp}, '1500');
+        }
+        //prevent page fom scrolling
+        return false;
+    });
 
 
 });
+
