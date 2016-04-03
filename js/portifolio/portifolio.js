77ffde320057c16d8f78fab37cc8fb0fd8bedd35
Raphael <ralmeida54@yahoo.com.br>
2016-04-03 11:02:21 -0300

    Efeitos bonitos ( fazendo ainda )
    - Retirei o onescroll, por enquanto

--- js/portifolio/portifolio.js
+++ js/portifolio/portifolio.js
@@ -1,15 +1,51 @@
+function oneScroll(w){
+    if( $(window).width() >= 768 ) {
+        //Function witch scrolls page to the next section
+        //Firefox
+        $(window).bind('DOMMouseScroll', function(e){
+            var vheight = $(window).scrollTop();
+            var sheight = $('section.heightParameter').height();
+            var totalDown = vheight + sheight;
+            var totalUp = vheight - sheight;
+            if(e.originalEvent.detail > 0) {
+                $('body').animate({scrollTop: totalDown}, '1500');
+            }else {
+                $('body').animate({scrollTop: totalUp}, '1500');
+            }
+            //prevent page fom scrolling
+            return false;
+        });
+
+        //Function witch scrolls page to the next section
+        //IE, Opera, Safari
+        $(window).bind('mousewheel', function(e){
+            var vheight = $(window).scrollTop();
+            var sheight = $('section.heightParameter').height();
+            var totalDown = vheight + sheight;
+            var totalUp = vheight - sheight;
+            if(e.originalEvent.wheelDelta < 0) {
+                $('body').animate({scrollTop: totalDown}, '1500');
+            }else {
+                $('body').animate({scrollTop: totalUp}, '1500');
+            }
+            //prevent page fom scrolling
+            return false;
+        });
+    }
+}
+
 $(document).ready(function(){
     //function which open and close slide menu
     $("#mediaNavButtonSlider").click(function() {
         $(this).parent("#mainNav").toggleClass("openMainNav");
     });
 
-    //Function which aplly a class when element is visible on the viewport
+    /*//Function which aplly a class when element is visible on the viewport
     $(window).scroll(function (e) {
         var scrollTop = $(window).scrollTop();
         var viewportHeight = $(window).height();
 
-        $('section:nth-child(n+2)').each(function () {
+        $(':nth-child(n+2)').each(function () {
             var top = $(this).offset().top;
             var bottom = top + $(this).height();
 
@@ -20,43 +56,42 @@ $(document).ready(function(){
                 $(this).removeClass('visible');
             }
         });
+    });*/
+
+
+
+    /*ANIMATE ON SCROLL*/
+    //Function which aplly a class (only one time) when element is visible on the viewport. ( this function does not removes class when element is no more visible )
+    $(window).scroll(function() {
+        $('section').each(function(){
+            var elementTopDistance = $(this).offset().top;
+            var scrollTopoDistance = $(window).scrollTop();
+
+            if (elementTopDistance < scrollTopoDistance +400) {
+                $(this).addClass("visibleEffects");
+            }
+        });
+    });
+
+
+    /*ANIMATE ON SCROLL*/
+    //Function which aplly a class (only one time) when element is visible on the viewport. ( this function does not removes class when element is no more visible )
+    $(window).scroll(function() {
+        $('.projectItem').each(function(){
+            var elementTopDistance = $(this).offset().top;
+            var scrollTopoDistance = $(window).scrollTop();
+
+            if (elementTopDistance < scrollTopoDistance +500) {
+                $(this).addClass("visibleEffects");
+            }
+        });
     });
 
 
 
 
-    //Function witch scrolls page to the next section
-    //Firefox
-    $(window).bind('DOMMouseScroll', function(e){
-        var vheight = $(window).scrollTop();
-        var sheight = $('section#cover').height();
-        var totalDown = vheight + sheight;
-        var totalUp = vheight - sheight;
-        if(e.originalEvent.detail > 0) {
-            $('body').animate({scrollTop: totalDown}, '1500');
-        }else {
-            $('body').animate({scrollTop: totalUp}, '1500');
-        }
-        //prevent page fom scrolling
-        return false;
-    });
 
-    //Function witch scrolls page to the next section
-    //IE, Opera, Safari
-    $(window).bind('mousewheel', function(e){
-        var vheight = $(window).scrollTop();
-        var sheight = $('section#cover').height();
-        var totalDown = vheight + sheight;
-        var totalUp = vheight - sheight;
-        if(e.originalEvent.wheelDelta < 0) {
-            $('body').animate({scrollTop: totalDown}, '1500');
-        }else {
-            $('body').animate({scrollTop: totalUp}, '1500');
-        }
-        //prevent page fom scrolling
-        return false;
-    });
-
-
+    $(window).scrollTop(0);
+    /*oneScroll();*/
 });
 
